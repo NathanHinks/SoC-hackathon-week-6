5d6f6ce0612380b1241ffe252ab77b04d6a2c3cf
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-02-12 12:24:23 +0000

    check winner works(ish)

--- src/App/App.js
+++ src/App/App.js
@@ -1,98 +1,90 @@
+import './App.css';
+import React, { useState } from 'react';
+import Board from '../Board';
 
-import "./App.css";
-import React, {useState} from "react";
-import Board from "../Board"
-
-const startBoard = [null, null, null, null, null, null, null, null, null]
+const startBoard = [ null, null, null, null, null, null, null, null, null ];
 
 function App() {
-  // nextPlayer state
-  const [isPlayerXTurn, setPlayerXTurn] = useState(true);
-  // board state
-  const [board, setBoard] = useState(startBoard);
-  //winner variable - reset each time someone plays
-
-  // function that takes in index and chages content of square
-  // takes in index that corresponds to which button (0 to 8)
-  //this happens on button click
+	// nextPlayer state
+	const [ isPlayerXTurn, setPlayerXTurn ] = useState(true);
+	// board state
+	const [ board, setBoard ] = useState(startBoard);
+  const winner = checkWinner(board);
+	//winner variable - reset each time someone plays
+	// function that takes in index and chages content of square
+	// takes in index that corresponds to which button (0 to 8)
+	//this happens on button click
   function fillSquare(index) {
-    // make a copy of board (index)
-    const boardCopy = [...board];
-    if (boardCopy[index] === null) {
-    //fill with x or o
-      if (isPlayerXTurn) {
-        //if x's turn fill with "X"
-        boardCopy[index] = "X"
-      }
-      else {
-        //else if o's turn, fill with an "O"
-        boardCopy[index] = "O"
-        }
+    console.log(winner)
+		// make a copy of board (index)
+		const boardCopy = [ ...board ];
+		//check for winner and if board square is empty
+    if (winner === null || boardCopy[index] === null) {
+			//fill with x or o
+			if (isPlayerXTurn) {
+				//if x's turn fill with "X"
+				boardCopy[index] = 'X';
+			}
+			else {
+				//else if o's turn, fill with an "O"
+				boardCopy[index] = 'O';
+			}
+		}
+		//if winner
+    if (winner !== null) {
+      alert(`${winner} YOU HAVE WON`);
     }
-    //else do nothing bc square is already filled
 
-    // set board to that board copy
-    setBoard(boardCopy);
-    //if checkWinner() !== null carry on....
-    //else {
-    // STOP PLAY 
-    //}
+		// set board to that board copy
+		setBoard(boardCopy);
+		// STOP PLAY
+		//}
+		// then swap the player using state above
+		setPlayerXTurn(!isPlayerXTurn);
+	}
 
-    // then swap the player using state above
-    setPlayerXTurn(!isPlayerXTurn)
-  }
+	//GET WINNNER
 
-  //GET WINNNER
-
-  //takes in board array
-function checkWinner(boardArray){
-  //array of possible wins each win will be an array eg: [1, 2, 3]
-  const possibleWins = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[0, 4, 8],
-		[2, 4, 6],
-	];
-  
-  //loop over that array
-    //for each possible win
-  // [1, 2, 3]
-  // wins.forEach(win => ) 
-  possibleWins.forEach(win => {
-    // destructure array
-  //const [a, b, c] = win
-    const [a, b, c] = win
-    //if statement to check if boardArray indexes for a, b and c are the same. eg all "X"
-    //if boardArray[a] !== null
-  // and boardArray[a] === b and c
-    //if it is, return the winner
-    if(boardArray[a] !== null && boardArray[a] === boardArray[b] && boardArray[a] === boardArray[c]){
-      return boardArray[a]
-    }
-    else{
-      //if not do nothing
-      //return null
-      return null
-    }
-  }
+	//takes in board array
+  function checkWinner(boardArray) {
+		//array of possible wins each win will be an array eg: [1, 2, 3]
+		const possibleWins = [
+			[ 0, 1, 2 ],
+			[ 3, 4, 5 ],
+			[ 6, 7, 8 ],
+			[ 0, 3, 6 ],
+			[ 1, 4, 7 ],
+			[ 2, 5, 8 ],
+			[ 0, 4, 8 ],
+			[ 2, 4, 6 ],
+		];
+    let winner = null;
+		//loop over that array
+		//for each possible win
+		// [1, 2, 3]
+		// wins.forEach(win => )
+		possibleWins.forEach((win) => {
+			// destructure array
+			//const [a, b, c] = win
+			const [ a, b, c ] = win;
+			//if statement to check if boardArray indexes for a, b and c are the same. eg all "X"
+			//if boardArray[a] !== null
+			// and boardArray[a] === b and c
+			//if it is, return the winner
+			if (boardArray[a] !== null && boardArray[a] === boardArray[b] && boardArray[a] === boardArray[c]) {
+				winner = boardArray[a];
+			}
+    })
     
-    )
-  
-  
-  
-  
-}
-  
-  return (
-    //PROPS TO SEND = fillSquare, board
-    <div className="App">
-      <Board fillSquare = {fillSquare} board = {board}/>  
-    </div>
-  );
+    return winner;
+	}
+
+	return (
+		//PROPS TO SEND = fillSquare, board
+		<div className='App'>
+			<Board fillSquare={fillSquare} board={board} winner={winner} />
+		</div>
+	);
 }
 
 export default App;
--- src/Board/index.js
+++ src/Board/index.js
@@ -2,11 +2,11 @@ import React, {useState} from "react";
 import Square from "../Square";
 
 //PROPS this needs fillsquares, board
-function Board({fillSquare, board}) {
+function Board({fillSquare, board, winner}) {
     // MAKE 9 squares by mapping over board
         //each squares PROPS: fillsquares, key(so React doesn't shout at us), index
     return <div className="board-display-container"> <div className = "current-player"> 
-        current player goes here
+        {winner}
         </div>
             <div className ="board">
             {board.map((item, index) => <Square index = {index} fillSquare={fillSquare} key={index} item = {item}/>)}
