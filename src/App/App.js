2582be3c381295b85a26ad3d84aecc3cc8f4855c
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-02-12 19:09:02 +0000

    all works

--- src/App/App.js
+++ src/App/App.js
@@ -5,52 +5,22 @@ import Board from '../Board';
 const startBoard = [ null, null, null, null, null, null, null, null, null ];
 
 function App() {
-	// nextPlayer state
 	const [ isPlayerXTurn, setPlayerXTurn ] = useState(true);
-	// board state
-  const [ board, setBoard ] = useState(startBoard);
-  const [ winnerDisplay, setWinnerDisplay] = useState("")
+	const [ board, setBoard ] = useState(startBoard);
   const winner = checkWinner(board);
-  
-	//winner variable - reset each time someone plays
-	// function that takes in index and chages content of square
-	// takes in index that corresponds to which button (0 to 8)
-	//this happens on button click
-  function fillSquare(index) {
-    
-		// make a copy of board (index)
+
+	function fillSquare(index) {
 		const boardCopy = [ ...board ];
-		//check for winner and if board square is empty
-    if (winner === null || boardCopy[index] === null) {
-			//fill with x or o
-			if (isPlayerXTurn) {
-				//if x's turn fill with "X"
-				boardCopy[index] = 'X';
-      }
-      
-			else {
-				//else if o's turn, fill with an "O"
-				boardCopy[index] = 'O';
-			}
-    }
-		//if winner
-		// set board to that board copy
+
+		if (winner === null && boardCopy[index] === null) {
+			isPlayerXTurn ? (boardCopy[index] = 'X') : (boardCopy[index] = 'O');
+		}
+
 		setBoard(boardCopy);
-		// STOP PLAY
-		// then swap the player using state above
 		setPlayerXTurn(!isPlayerXTurn);
-    
-    if (winner !== null) {
-      setWinnerDisplay("The winner is: " + winner)
-      return;
-    }
 	}
 
-	//GET WINNNER
-
-	//takes in board array
-  function checkWinner(boardArray) {
-		//array of possible wins each win will be an array eg: [1, 2, 3]
+	function checkWinner(boardArray) {
 		const possibleWins = [
 			[ 0, 1, 2 ],
 			[ 3, 4, 5 ],
@@ -61,38 +31,26 @@ function App() {
 			[ 0, 4, 8 ],
 			[ 2, 4, 6 ],
 		];
-    let currentWinner = null;
-		//loop over that array
-		//for each possible win
-		// [1, 2, 3]
-		// wins.forEach(win => )
-		possibleWins.forEach((win) => {
-			// destructure array
-			//const [a, b, c] = win
-			const [ a, b, c ] = win;
-			//if statement to check if boardArray indexes for a, b and c are the same. eg all "X"
-			//if boardArray[a] !== null
-			// and boardArray[a] === b and c
-			//if it is, return the winner
-			if (boardArray[a] !== null && boardArray[a] === boardArray[b] && boardArray[a] === boardArray[c]) {
-        currentWinner = boardArray[a];
-			//alert(`${currentWinner} wins!!!!`)
-      }
-    })
+		let currentWinner = null;
 
-    return currentWinner;
-  }
-  
-  function handleReset() {
-    const resetBoard = [...startBoard];
-    setBoard(resetBoard);
-    setWinnerDisplay("");
-  }
+		possibleWins.forEach((win) => {
+			const [ a, b, c ] = win;
+			if (boardArray[a] !== null && boardArray[a] === boardArray[b] && boardArray[a] === boardArray[c]) {
+				currentWinner = boardArray[a];
+			}
+		});
+    //because can't return in forEach
+		return currentWinner;
+	}
+
+	function handleReset() {
+		const resetBoard = [ ...startBoard ];
+		setBoard(resetBoard);
+	}
 
 	return (
-		//PROPS TO SEND = fillSquare, board
 		<div className='App'>
-      <Board fillSquare={fillSquare} winnerDisplay={winnerDisplay} board={board} handleReset={handleReset}/>
+			<Board fillSquare={fillSquare} board={board} handleReset={handleReset} winner={winner} isPlayerXTurn={isPlayerXTurn} />
 		</div>
 	);
 }
--- src/Board/index.js
+++ src/Board/index.js
@@ -1,20 +1,24 @@
-import React, {useState} from "react";
-import Square from "../Square";
+import React from 'react';
+import Square from '../Square';
 
 //PROPS this needs fillsquares, board
-function Board({fillSquare, board, winnerDisplay, handleReset}) {
-    // MAKE 9 squares by mapping over board
-        //each squares PROPS: fillsquares, key(so React doesn't shout at us), index
-    return <div className="board-display-container"> <div className = "current-player"> 
-        <span> {winnerDisplay}</span>
-        </div>
-            <div className ="board">
-            {board.map((item, index) => <Square index = {index} fillSquare={fillSquare} key={index} item = {item}/>)}
-        </div> 
-        <button className="reset-btn" onClick={handleReset}>Reset</button>
-    </div>
-    
-    
+function Board({ fillSquare, board, winner, isPlayerXTurn, handleReset }) {
+	return (
+		<div className='board-display-container'>
+			{' '}
+			<div className='current-player'>
+                {winner ?
+                    `Winner is: ${winner}` :
+                    `Next Player: ${isPlayerXTurn ? 'X' : 'O'}`}
+			</div>
+			<div className='board'>
+				{board.map((item, index) => <Square index={index} fillSquare={fillSquare} key={index} item={item} />)}
+			</div>
+			<button className='reset-btn' onClick={handleReset}>
+				Reset
+			</button>
+		</div>
+	);
 }
 
 export default Board;
