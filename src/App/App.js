7e308a7393defcf607884092142a82cb1697c1f6
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-02-12 10:57:03 +0000

    fillsquare function

--- src/App/App.js
+++ src/App/App.js
@@ -3,21 +3,44 @@ import "./App.css";
 import React, {useState} from "react";
 import Board from "../Board"
 
+const startBoard = [null, null, null, null, null, null, null, null, null]
+
 function App() {
   // nextPlayer state
+  const [isPlayerXTurn, setPlayerXTurn] = useState(true);
   // board state
+  const [board, setBoard] = useState(startBoard);
 
   // function that takes in index and chages content of square
-   // takes in index that corresponds to which button (0 to 8)
-   // make a copy of board (index)
-   // access that particular square using the array 
-   // if already filled, do nothing, else just fill with x or o
-   // set board to that board copy 
-   // then swap the player using state above
+  // takes in index that corresponds to which button (0 to 8)
+  //this happens on button click
+  function fillSquare(index) {
+    // make a copy of board (index)
+    const boardCopy = [...board];
+    if (boardCopy[index] === null) {
+    //fill with x or o
+      if (isPlayerXTurn) {
+        //if x's turn fill with "X"
+        boardCopy[index] = "X"
+      }
+      else {
+        //else if o's turn, fill with an "O"
+        boardCopy[index] = "O"
+        }
+    }
+    //else do nothing bc square is already filled
+
+    // set board to that board copy
+    setBoard(boardCopy);
+    // then swap the player using state above
+    setPlayerXTurn(!isPlayerXTurn)
+  }
+
   
   return (
+    //PROPS TO SEND = fillSquare, board
     <div className="App">
-      <Board/>
+      <Board />  
     </div>
   );
 }
--- src/Board/index.js
+++ src/Board/index.js
@@ -1,8 +1,11 @@
 import React, {useState} from "react";
 import Square from "../Square";
 
+//PROPS this needs fillsquares, board
 function Board() {
-    return <div> <div className = "current-player"> 
+    // MAKE 9 squares by mapping over board
+        //each squares PROPS: fillsquares, key(so React doesn't shout at us), index
+    return <div className="board-display-container"> <div className = "current-player"> 
         current player goes here
         </div>
         <div className = "borad-row row-1" > 
@@ -19,7 +22,7 @@ function Board() {
         <Square index = {6}/>
         <Square index = {7}/>
         <Square index = {8}/>
-        </div>
+        </div> 
         </div>
     
 }
