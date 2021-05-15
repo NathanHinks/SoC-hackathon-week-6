5f930f2490be6106a47b6f78a5def0725a8dfc41
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-05-15 14:13:41 +0100

    random player after every reset

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,23 +1,27 @@
 import { startBoard } from '../libs/gameVars';
-import {takeTurn, randomizeFirstPlayer} from './utils';
+import { randomizeFirstPlayer, takeTurn } from './utils';
 
 const initialState = {
-    board: startBoard,
-    isPlayerXTurn: randomizeFirstPlayer(),
-    winner: null
-}
+  board: startBoard,
+  isPlayerXTurn: randomizeFirstPlayer(),
+  winner: null,
+};
 
 const gameReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'TAKE_TURN':
-        const updatedState = takeTurn(state, action.payload)
+      const updatedState = takeTurn(state, action.payload);
+
       return {
         ...state,
         ...updatedState,
         isPlayerXTurn: !state.isPlayerXTurn,
       };
     case 'RESET':
-      return initialState;
+      return {
+        ...initialState,
+        isPlayerXTurn: randomizeFirstPlayer(),
+      };
     default:
       return state;
   }
